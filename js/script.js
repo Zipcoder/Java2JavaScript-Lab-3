1442a7c3abbb3f2bf9bbfff4ce706b6acec8ec66
Zihao Castine <zihaocastine@Zipcoder02s-MacBook-Pro.local>
2016-05-19 15:25:06 -0400

    part 1and 2

--- js/script.js
+++ js/script.js
@@ -1,28 +1,49 @@
 " use strict ";
-
+var phoneBookmap={};
 function PhoneBook(){
 
 };
 
 function listAllNames() {
   // code to list all names
+  var allNames="";
+  for(var name in phoneBookmap){
+    allNames+=name+"</br>"
+  }
+  display.innerHTML=allNames;
 };
 
 function listAllNumbers() {
   // code to list all numbers
+  var allNumbers="";
+  for(var each in phoneBookmap){
+    allNumbers+=phoneBookmap[each]+"</br>";
+  }
+  display.innerHTML=allNumbers;
 };
 
 function showAdd() {
   var name = prompt("Enter full name");
   var number = prompt("Enter phone number");
+  phoneBookmap[name]=number;
 };
 
 function showRemove() {
   var remove = prompt("Enter name to remove");
+  delete phoneBookmap[remove];
 };
 
 function showLookup() {
-  var lookup = prompt("Enter number to lookup");
+  var lookup = prompt("Enter name to lookup");
+  display.innerHTML=phoneBookmap[lookup];
+};
+function showReverseLookup(){
+  var reverseLookup = prompt("Enter number to lookup");
+  for(var each in phoneBookmap){
+    if(reverseLookup==phoneBookmap[each]){
+      display.innerHTML=each;
+    }
+  }
 };
 
 var display = document.getElementById("display");
